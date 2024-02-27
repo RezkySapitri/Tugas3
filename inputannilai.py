c129fb6ec6797f5d68cb1ed8234525d814b4af9f
RezkySapitri <rezkysapitri03@gmail.com>
2024-02-27 20:25:06 +0700

    Mengumpulkan tugas sesi3

---
+++ inputannilai.py
@@ -0,0 +1,5 @@
+nilai = int (input ("Masukan nilai anda :"))
+if nilai > 75:
+    print("Selamat anda lulus") 
+else:
+    print ("maaf anda tidak lulus")
---
+++ konversi.py
@@ -0,0 +1,14 @@
+nilai = int(input("masukan nilai anda :"))
+
+if nilai >= 0 and nilai <=50:
+    print ("nilai E")
+elif nilai >= 50 and nilai <=60:
+    print ("nilai D")  
+elif nilai >= 60 and nilai <=70:
+    print ("nilai C")    
+elif nilai >= 70 and nilai <=80:
+    print ("nilai B")    
+elif nilai >= 80 and nilai <=100:
+    print ("nilai A")
+else :
+    print ("nilai tidak valid")
